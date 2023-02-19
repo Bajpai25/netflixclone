7ae6a07dc52bd1cab2d9a20d58cd01ab10b0ec7b
Shashwat bajpai <bajpai.shashwat.332@gmail.com>
2023-02-20 00:57:47 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Navbar.css";
+ const Navbar = () => {
+  return (
+    <div className='nav'>
+      <img className='nav_logo'
+      src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png"
+      width={80} height={40} 
+      alt="netflix"/> 
+    </div>
+  )
+}
+export default Navbar;
